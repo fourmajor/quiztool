506e18da1fe8dd1bed51ee6cfc17bce89aba0bb3
Stu Chuang Matthews <stu@fourmajor.com>
2017-02-21 10:44:31 -0800

    added question detail section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,13 @@ const QUESTIONS: Question[] = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'quiz tool2';
-  question: Question = {
-  	id: 1,
-  	question: 'wh3at is the square root of two?',
-  	answer: 'approximately 1.41'
-  };
+  selectedQuestion: Question;
   questions = QUESTIONS;
+  onSelect(question: Question) void {
+    this.selectedQuestion = question;
+  }
 }
+
