66296baa654d837b8a8b016e48738bae60ba6f18
Stu Chuang Matthews <stu@fourmajor.com>
2017-02-21 09:50:52 -0800

    fixed syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ export class Question {
 	answer: string;
 }
 
+const QUESTIONS: Question[] = [
+  {id:2, question: 'What is shmooth\'s favorite player?', answer: 'Derek Fisher for some reason.'},
+  {id:3, question: 'What is fourmajor\'s favorite player?', answer: 'Obviously J Wall'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,13 +18,10 @@ export class Question {
 })
 export class AppComponent {
   title = 'quiz tool2';
-  question: Question = {
+  question Question = {
   	id: 1,
   	question: 'wh3at is the square root of two?',
   	answer: 'approximately 1.41'
   }
+  questions = QUESTIONS;
 }
-
-const QUESTIONS: Question[] = [
-  {id:2, question: 'What is shmooth\'s favorite player?', answer: 'Derek Fisher for some reason.'},
-  {id:3, question: 'What is fourmajor\'s favorite player?', answer: 'Obviously J Wall'}]
