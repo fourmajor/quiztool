5ffd764691ab095a47ada3a262b49993723966a4
Stu Matthews <stu@wh-dev01.localdomain>
2017-03-02 15:19:53 +0000

    fixed?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,8 @@ export class Question {
 }
 
 const QUESTIONS: Question[] = [
-<<<<<<< HEAD
-  {id:1, question: 'What is the purpose of this quiz?', answer: 'The Wizards are good-ish all of a sudden.'},
-  {id:2, question: 'What is shmooth\'s favorite player?', answer: 'Derek Fisher for some reason.'},
-=======
   {id:1, question: 'Should we build a great quiz tool?', answer: 'Yes. This has been another edition of Simple Answers to Simple Questions.'},
   {id:2, question: 'What is shmooth\'s favorite player??', answer: 'Derek Fisher for some reason.'},
->>>>>>> 1ad79d3c318c71a0cbd9d189b1e50edb24a6033f
   {id:3, question: 'What is fourmajor\'s favorite player?', answer: 'Obviously J Wall'}
 ];
 
