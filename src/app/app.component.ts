ea4c37d204ecda27d3907236bdbd0f93c04e8540
Peter Smith <psmithsf@gmail.com>
2017-03-03 03:20:18 -0800

    ui fixes mostly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 export class Question {
 	id: number;
 	question: string;
+    useranswer: string;
 	answer: string;
 }
 
 const QUESTIONS: Question[] = [
-  {id:1, question: 'Should we build a great quiz tool?', answer: 'Yes. This has been another edition of Simple Answers to Simple Questions.'},
+  {id:1, question: 'Should we build a great quiz tool?', answer: 'Yes. This has been another edition of Simple Answers to Simple Questions. That was a pretty great question, methinks.'},
   {id:2, question: 'What is shmooth\'s favorite player??', answer: 'Derek Fisher for some reason.'},
   {id:3, question: 'What is fourmajor\'s favorite player?', answer: 'Obviously J Wall'}
 ];
