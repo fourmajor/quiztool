c41ec99bddefbf7ec3fbf46cc8ae421f2c3886be
Peter Smith <psmithsf@gmail.com>
2017-03-07 09:07:42 -0800

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,54 @@
 import { Component } from '@angular/core';
 
+
 export class Question {
 	id: number;
 	question: string;
-    useranswer: string;
 	answer: string;
+    useranswer: string;
+    quizid: number;
     /* type: text, multiple choice, multiple-choice only one answer, image, drop-down, etc. */
     /* Will we have subclasses? */
 }
 
 const QUESTIONS: Question[] = [
-  {id:1, question: 'Should we build a great quiz tool?', answer: 'Yes. This has been another edition of Simple Answers to Simple Questions. That was a pretty great question, methinks.', useranswer: ''},
-/*  {id:2, question: 'What is shmooth\'s favorite player??', answer: 'Derek Fisher for some reason.', useranswer: ''},
-  {id:3, question: 'What is fourmajor\'s favorite player?', answer: 'Obviously J Wall', useranswer: ''}*/
+  {id:1, question: 'Should we build a great quiz tool?', answer: 'Yes. This has been another edition of Simple Answers to Simple Questions. That was a pretty great question, methinks.', useranswer: '', quizid: 1}
+  /* {id:2, question: 'What is shmooth\'s favorite player??', answer: 'Derek Fisher for some reason.', useranswer: ''}*/
+  /*{id:3, question: 'What is fourmajor\'s favorite player?', answer: 'Obviously J Wall', useranswer: ''}*/
 ];
 
+export class Quiz {
+    // has the Quiz been evaluated and scored?
+    wasEvaluated: boolean; // why not just set it here?
+
+    questions: Question[]; // get the list of questions from statis data, or service/db, etc.
+
+    selectedQuestion: Question;
+
+    constructor() { 
+        this.wasEvaluated = false;
+        this.questions = QUESTIONS;
+    }
+
+}
+
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    //template:`<h2>Hello, world</h2>` ,
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'Quiz Tool';
-  selectedQuestion: Question;
-  questions = QUESTIONS;
-  onSelect(question: Question): void {
-    this.selectedQuestion = question;
-  }
+    title = 'Quiz Tool';
+    quiz = new Quiz();
+
+    doAlert(){
+    }
+        
+    onSelect(question: Question): void {
+        this.quiz.selectedQuestion = question;
+    }
 }
 
