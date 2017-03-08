57fe659274adc017305703c407be81167fb46a45
Peter Smith <psmithsf@gmail.com>
2017-03-08 04:57:21 -0800

    added font-awesome; more formatting.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,57 @@ export class Question {
 	answer: string;
     useranswer: string;
     quizid: number;
+    quiz: Quiz; // include ref back to parent object, the Quiz
+
+    wasCorrect(){
+
+        //this.quiz.log('useranswer: ' + this.useranswer);
+        //log('answer: ' + this.answer);
+
+        if (this.useranswer == this.answer){
+            return true;
+        }
+        return false;
+    }
+
     /* type: text, multiple choice, multiple-choice only one answer, image, drop-down, etc. */
     /* Will we have subclasses? */
 }
-
-const QUESTIONS: Question[] = [
-  {id:1, question: 'Should we build a great quiz tool?', answer: 'Yes. Stu.', useranswer: '', quizid: 1},
-  {id:2, question: 'What is shmooth\'s favorite player??', answer: 'Yes. Peter.', useranswer: '', quizid: 1}
-  /*{id:3, question: 'What is fourmajor\'s favorite player?', answer: 'Obviously J Wall', useranswer: 'testua3'}*/
-];
 
 export class Quiz {
     // has the Quiz been evaluated and scored?
     wasEvaluated: boolean; // why not just set it here?
     questions: Question[]; // get the list of questions from statis data, or service/db, etc.
-    selectedQuestion: Question;
-
+    selectedQuestion: Question; // not using this at the moment
 
     constructor() { 
         this.wasEvaluated = false;
-        this.questions = QUESTIONS;
+
+        // these will eventualy come from db/service/etc. using static JSON data struct init didn't work
+        // once i added a function to Question class - typescript sucks :)
+
+        // init new array
+        this.questions = [];
+
+        var q = new Question();
+        q.id = 1;
+        q.question = "Should we build a great quiz tool?";
+        q.answer = "Yes";
+        q.quizid = 1;
+        q.quiz = this;
+        this.questions.push(q); // will this blow up?
+/*
+        q = new Question();
+        q.id = 1;
+        q.question = "What is shmooth\'s favorite player?";
+        q.answer = "No";
+        q.quizid = 1;
+        q.quiz = this;
+        this.questions.push(q);
+*/
     }
+ 
+
 
     /**
     * Score the quiz.
@@ -38,17 +68,7 @@ export class Quiz {
         
         // for each question on the quiz, check if the provided answer is correct
         for (let question of this.questions) {
-            //if (QuestionAnsweredCorrectly()){
-            //}
             console.log(question); // 1, "string", false
-            if (question.useranswer == question.answer){
-                console.log('Correct!');
-            }
-            else{
-                console.log('Incorrect.');
-                console.log('useranswer: ' + question.useranswer);
-                console.log('answer: ' + question.answer);
-            }
         }
         this.wasEvaluated=true;
     }
@@ -66,18 +86,9 @@ export class AppComponent {
     testVar: String = "peter is cool";
 
     /**
-    * Evaluate (score) the quiz. 
-    */
-    evaluate(){
-        this.log('Evaluating...');
-        console.log('testVar is ' + this.testVar);
-        this.quiz.evaluate();
-    }
-
-    /**
     * Simple wrapper function to log stuff to the javascript console.
     */
-    log(message: String){
+    public log(message: String){
         console.log( message ); // black
         /*
         console.debug('debug: test log, yo...'); // blue
