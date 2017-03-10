75654c9c42addcc14af006712631bf324190aba6
shadowjs <mickadams@outlook.fr>
2017-03-10 21:39:06 +0100

    Focus with ViewChild
    ElementRef allow direct DOM manipulation, that's must be avoided (security risk). Use ViewChild instead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 
 
 export class Question {
@@ -85,17 +85,24 @@ export class AppComponent {
     quiz = new Quiz();
     testVar: String = "peter is cool";
 
-    constructor(
-        private element: ElementRef
-    ){}
+    @ViewChild('focusThis') focusThis;
+    
+    //O to focus
+    id:number=0
+
+    constructor(){}
 
     newQuiz() {
         this.quiz = new Quiz();
+        this.id=0
     }
 
     //wait for viewchild to update then focus
     ngAfterViewChecked(){
-        this.element.nativeElement.querySelectorAll('textarea')[0].focus()
+        if(this.id == 0){
+            this.focusThis.nativeElement.focus();
+            this.id++
+        }
     }
 
     /**
