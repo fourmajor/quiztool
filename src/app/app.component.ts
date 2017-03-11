958840cb5bd1687220d67d41c0633e2e86e4fdb9
Peter Smith <psmithsf@gmail.com>
2017-03-11 03:18:54 -0800

    added scoring/success/fail with error msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,10 @@ export class Question {
     	}
     	return false;
     }
+	// i kind of like this name better
+	wasAnsweredCorrectly(){
+		return this.wasCorrect();
+	}
 }
 
 export class Quiz {
@@ -22,8 +26,22 @@ export class Quiz {
     wasEvaluated: boolean; // why not just set it here?
     questions: Question[]; // get the list of questions from statis data, or service/db, etc.
     selectedQuestion: Question; // not using this at the moment
+	pass_percentage: number;
+	score_percentage: number;
+	passed: boolean; // did user pass the quiz?
+	quiz_done_status_message: String;
+
+	userPassed(){
+		if (this.score_percentage >= this.pass_percentage) {
+			return true;
+		}
+		return false;
+	}
 
     constructor() { 
+		this.pass_percentage = 90; // % required to pass a quiz
+		this.score_percentage = 0; // use did not score anything yet, but we default to 0
+
         this.wasEvaluated = false;
 
         // these will eventualy come from db/service/etc. using static JSON data struct init didn't work
@@ -55,9 +73,27 @@ export class Quiz {
     evaluate(){
         //console.log('Evaluating from within Quiz object...');
         // for each question on the quiz, check if the provided answer is correct
+		var num_right: number = 0;
+		var num_wrong: number = 0;
         for (let question of this.questions) {
             //console.log(question); // 1, "string", false
+			if ( question.wasAnsweredCorrectly() ){ 
+				++num_right; 
+			} else {
+				++num_wrong; 
+			}
         }
+	    var total_number_of_questions = this.questions.length;
+		console.log('num questions: ' + total_number_of_questions);
+		this.score_percentage = Math.floor( (num_right/total_number_of_questions)*100 );
+		console.log('score_percentage: ' + this.score_percentage);
+	
+		if (this.userPassed()){
+			this.quiz_done_status_message = "Good job!";
+		} else {
+			this.quiz_done_status_message = "That's ok -- plenty more where that came from.";
+		}
+	
         this.wasEvaluated=true;
     }
 }
@@ -71,18 +107,15 @@ export class Quiz {
 })
 export class AppComponent {
     title: String = 'Quiz Tool';
-	//cdr: ChangeDetectorRef;
     quiz = new Quiz();
 
     @ViewChild('focusThis') focusThis;
     @ViewChild('continueButton') continueButton;
     
     //O to focus
-    id:number=0
+    id:number=0;
 
- 	//constructor(cdr: ChangeDetectorRef) {
  	constructor() {
-    	//this.cdr = cdr;
   	}
 
     newQuiz() {
@@ -92,7 +125,6 @@ export class AppComponent {
 
     //wait for viewchild to update then focus
     ngAfterViewChecked(){
-    	//this.cdr.detectChanges();
         if( (!this.quiz.wasEvaluated) && (this.id == 0) ){
 			// this is a crazy hack b/c Angular sucks
 			setTimeout(() => {
@@ -105,10 +137,7 @@ export class AppComponent {
 			setTimeout(() => {
 				this.continueButton.nativeElement.focus(); // then focus us on the 'Continue' button
     		}, 1);
-			//console.log(this.continueButton);
 		}
-    	//this.cdr.detectChanges();
-		//console.log('document.activeElement.id: ' + document.activeElement.id);
     }
 
     /**
@@ -116,12 +145,6 @@ export class AppComponent {
     */
     public log(message: String){
         console.log( message ); // black
-        /*
-        console.debug('debug: test log, yo...'); // blue
-        console.warn('warn: test log, yo...'); // yellow
-        console.info('info: test log, yo...'); // black (with 'i' icon indicator)
-        console.error('error: test log, yo...'); // red
-        */
     }
 
     alert(message: String){
