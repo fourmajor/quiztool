5661f05ed2042fa2748bd87ff7306dc71db3b13b
Peter Smith <psmithsf@gmail.com>
2017-03-19 22:40:29 -0700

    fixed focus issues mainly prob

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,14 @@ export class Question {
     	}
     	return false;
     }
+
+	wasAnswered(){
+		if(this.useranswer){
+			return false;
+		}
+		return true;
+	}
+
 	// i kind of like this name better
 	wasAnsweredCorrectly(){
 		return this.wasCorrect();
@@ -26,6 +34,7 @@ export class Quiz {
 	start: number = 50;
     // has the Quiz been evaluated and scored?
     wasEvaluated: boolean; // why not just set it here?
+    isActive: boolean; // why not just set it here?
     questions: Question[]; // get the list of questions from statis data, or service/db, etc.
     selectedQuestion: Question; // not using this at the moment
 	pass_percentage: number;
@@ -65,15 +74,20 @@ export class Quiz {
 
 	evaluateThisQuestion(event){
 		if (this.question_index>= (this.questions.length - 1) ){
-			console.log('out of questions! evaluating quiz now.');
+			//console.log('out of questions! evaluating quiz now.');
 			this.evaluate();
 		}
 		else{
 			this.nextQuestion();
-			console.log('We are evaluating this question...' + event);
-			event.stopPropagation();
-			return false;
+			//console.log('We are evaluating this question...' + event);
+			//event.stopPropagation();
 		}
+		
+		if(event){ // event not here for clicks, apparently?
+			event.stopPropagation(); // don't allow the 'eval'/enter event to be processed twice
+									 // else the 'review' screen at end will be skipped
+		}
+		return false;
 	}
 
 	/**
@@ -83,6 +97,7 @@ export class Quiz {
 		if (this.question_index>= (this.questions.length - 1) ){
 			console.log('out of questions! evaluating quiz now.');
 			this.evaluate();
+			this.isActive = false; // mark the quiz as 'not running'
 		} else {
 			console.log('here...');
 			this.getCurrentQuestion().useranswer='';
@@ -126,8 +141,16 @@ export class Quiz {
 
         q = new Question();
         q.id = 1;
-        q.question = "What is shmooth\'s favorite player?";
-        q.answer = "no";
+        q.question = "Does Donald Trump suck?";
+        q.answer = "yes";
+        q.quizid = 1;
+        q.quiz = this;
+        this.questions.push(q);
+
+        q = new Question();
+        q.id = 1;
+        q.question = "Is this a third question?";
+        q.answer = "yes";
         q.quizid = 1;
         q.quiz = this;
         this.questions.push(q);
@@ -150,65 +173,144 @@ export class Quiz {
 			}
         }
 	    var total_number_of_questions = this.questions.length;
-		console.log('num questions: ' + total_number_of_questions);
+		//console.log('num questions: ' + total_number_of_questions);
 		this.score_percentage = Math.floor( (num_right/total_number_of_questions)*100 );
-		console.log('score_percentage: ' + this.score_percentage);
+		//console.log('score_percentage: ' + this.score_percentage);
 	
 		if (this.userPassed()){
-			this.quiz_done_status_message = "Good job!";
+			this.quiz_done_status_message = "Great job -- you passed!";
+		}
+		else if (num_right > 0){
+			this.quiz_done_status_message = "Well, you got at least one right. :) Try again!";
 		} else {
 			this.quiz_done_status_message = "That's ok -- plenty more where that came from.";
 		}
 	
         this.wasEvaluated=true;
+		this.isActive=false;
     }
 }
 
 @Component({
+
+	host: {
+        '(document:keydown)': 'handleKeyboardEvents($event)'
+    },
+
     selector: 'app-root',
     templateUrl: './app.component.html',
     //template:`<h2>Hello, world</h2>` ,
     styleUrls: ['./app.component.css'],
-	changeDetection: ChangeDetectionStrategy.OnPush, // not sure if still needed; see setTimeout() hack
+	//changeDetection: ChangeDetectionStrategy.OnPush, // not sure if still needed; see setTimeout() hack
 })
 export class AppComponent {
+
     title: String = 'Quiz Tool';
     quiz: Quiz;
-
+	focusSet:boolean = false;
+	focused:number = 0;
 	start: number = 50;
+
+	handleKeyboardEvents(event: KeyboardEvent) {
+        if(event.keyCode==13){
+			//console.log('we got an enter keydown...' + event.keyCode);
+
+			if(this.quiz.isActive){
+				// then why is this being called?
+				console.log('the current quiz is active');
+			}
+			else{
+				console.log('the current quiz is not active');
+        		//this.quiz.evaluateThisQuestion(event);
+				this.newQuiz();
+			}
+
+			return false;
+			// RED_FLAG: we need a way to know if this quiz was _just_ evaluated, or if it's at some other stage now.
+			// shiiiiiiiiit
+			//this.focusSet=false;
+
+			//if(this.quiz.wasEvaluated && (this.quiz.isActive) ){
+				//this.newQuiz();
+			//	return false;
+			//}
+		}
+    }
+
 
     @ViewChild('focusThis') focusThis;
     @ViewChild('continueButton') continueButton;
+    @ViewChild('nextQuizButton') nextQuizButton;
     
     //O to focus
     id:number=0;
 
  	constructor() {
-
 		this.quiz = new Quiz();
-
   	}
 
     newQuiz() {
         this.quiz = new Quiz();
+		this.quiz.isActive = true;
+		this.focusSet = false;
+		this.focused = 0;
         this.id=0
     }
 
-    //wait for viewchild to update then focus
+
+
+    // this is seemingly called every time the view is effected, including every keystroke, entering text, etc.
     ngAfterViewChecked(){
-        if( (!this.quiz.wasEvaluated) && (this.id == 0) ){
+
+		console.log('ngAfterViewChecked() is being called...');
+
+		console.log('value of focused is ' + this.focused);
+
+		if( (! this.focusSet) && (this.focused < 10) ){
+				setTimeout(() => {
+					this.focusThis.nativeElement.focus();
+					this.focusSet = true;
+					this.focused++;
+					//this.id++;
+				}, 1);
+		}
+		else{
+			console.log('focus is set...already.');
+		}
+			
+	}
+
+    ngAfterViewInit(){
+		console.log('ngAfterViewInit()...');
+        //if( (!this.quiz.wasEvaluated) && (this.id == 0) ){
+        if( (!this.quiz.wasEvaluated) ){
 			// this is a crazy hack b/c Angular sucks
 			setTimeout(() => {
             	this.focusThis.nativeElement.focus();
-            	this.id++;
+            	//this.id++;
     		}, 1);
         }
+
+		/*
+		if (this.quiz.wasEvaluated && (this.quiz.quiz_display_type == this.quiz.ONE_QUESTION_AT_A_TIME) ){
+			// this is a crazy hack b/c Angular sucks
+			setTimeout(() => {
+				console.log('here...');
+				//this.nextQuizButton.nativeElement.focus(); // then focus us on the 'Continue' button
+    		}, 10);
+		}
+		*/
+		
+		/*
 		if (this.quiz.wasEvaluated){
 			// this is a crazy hack b/c Angular sucks
 			setTimeout(() => {
+				console.log('focusing on continueButton...');
 				this.continueButton.nativeElement.focus(); // then focus us on the 'Continue' button
-    		}, 1);
+    		}, 1000);
 		}
+		*/
+	
     }
 
     /**
